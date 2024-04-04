06840f1c64710aacd79f8a119e902e0263215357
Álvaro Laguna <72698468+alvarola17@users.noreply.github.com>
2024-04-04 09:10:42 +0200

    Refactor load_contaminacion_acustica function to filter and write CSV files based on date range

--- dags/contaminacion_acustica_dag.py
+++ dags/contaminacion_acustica_dag.py
@@ -35,7 +35,7 @@ def extract_contaminacion_acustica(url):
     return df
 
 
-def load_contaminacion_acustica(df, folder_path, filename):
+def load_contaminacion_acustica(df, folder_path):
     """
     Carga el CSV incremental correspondiente en la carpeta de información cruda "/raw"
     """
@@ -47,32 +47,47 @@ def load_contaminacion_acustica(df, folder_path, filename):
     # For Debugging Purposes
     print(os.getcwd())
 
-    # Construct the full file path
-    file_path = os.path.join(folder_path, filename)
+    start_date = pd.to_datetime(str(default_args["start_date"])[:10], format="%Y-%m-%d")
 
     # Filter DataFrame by date
-    todays_date = pendulum.now(tz="UTC").date()
-    # Knowing that todays date is on monday, substract 1 day to get the last sunday
-    last_sunday = str(todays_date.subtract(days=1))[:10]
-    last_monday = str(todays_date.subtract(days=7))[:10]
-    print(f"Last Sunday: {last_sunday}")
-    print(f"Last Monday: {last_monday}")
+    run_date = pd.to_datetime(os.environ.get("AIRFLOW_CTX_EXECUTION_DATE")[:10])
+    # Get the csvs inside /opt/airflow/raw
+    files = os.listdir(folder_path)
+    # Filter the csv that strats with 'contaminacion_acustica'
+    files = [file for file in files if file.startswith("contaminacion_acustica")]
+    # Get the dates from the csvs
+    dates = [
+        "".join(file.split("_")[2:]).split(".")[0].replace("_", "-") for file in files
+    ]
+    # Convert to pandas datetime
+    dates = pd.to_datetime(dates)
+    print(f"Dates: {dates}")
+    # Get the last date
+    last_date = dates.max()
+    if last_date is pd.NaT:
+        last_date = start_date
 
-    # Get date from anio, mes, dia columns
-    df["fecha"] = pd.to_datetime(
-        df[["anio", "mes", "dia"]].rename(
-            columns={"anio": "year", "mes": "month", "dia": "day"}
-        )
-    )
+    print(f"Last date: {last_date}")
+    print(f"Run date: {run_date}")
 
-    # Filter by date
-    df = df[(df["fecha"] >= last_monday) & (df["fecha"] <= last_sunday)]
-
-    df.drop(columns=["fecha"], inplace=True)
-
-    # Write DataFrame to CSV
-    df.to_csv(file_path, index=False)
-    print(f"DataFrame written to {file_path}")
+    # For loop from last_date to run_date
+    for date in pd.date_range(start=last_date, end=run_date, freq="D"):
+        print(f"Processing date: {date}")
+        print(f"Year: {date.year}")
+        print(f"Month: {date.month}")
+        print(f"Day: {date.day}")
+        # Construct the full file path
+        assert date.month != 1
+        timestamp_str = (str(date)[:10]).replace("-", "_")
+        filename = f"contaminacion_acustica_{timestamp_str}.csv"
+        file_path = os.path.join(folder_path, filename)
+        # Write DataFrame to CSV
+        df_filtered = df[
+            (df.anio == date.year) & (df.mes == date.month) & (df.dia == date.day)
+        ]
+        if not df_filtered.empty:
+            df_filtered.to_csv(file_path, index=False)
+            print(f"DataFrame written to {file_path}")
 
 
 default_args = {
@@ -84,10 +99,11 @@ default_args = {
 
 with DAG(
     dag_id="contaminacion_acustica",
-    schedule_interval="35 23 * * 1",
+    schedule_interval="35 23 * * *",
     tags=["Ayuntamiento_Madrid"],
     default_args=default_args,
 ) as dag:
+    import os
 
     @task(task_id="extract_from_url")
     def extract():
@@ -105,14 +121,11 @@ with DAG(
 
         # Define the folder path
         folder_path = "/opt/airflow/raw"
+        print(f"Start_date {dag.__dict__}")
+        print(str(default_args["start_date"])[:10])
+        print(str(default_args["start_date"]))
 
-        # Generate a unique filename based on the current timestamp
-        timestamp_str = (str(pendulum.now(tz="UTC"))[:10]).replace("-", "_")
-        print(timestamp_str)
-        filename = f"contaminacion_acustica_{timestamp_str}.csv"
-        print(filename)
-
-        load_contaminacion_acustica(df, folder_path, filename)
+        load_contaminacion_acustica(df, folder_path)
 
     extract_task = extract()
 

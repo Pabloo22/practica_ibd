ab41cdd5670ff6eb99d072695c81e8642f14025f
Pabloo22 <pablete.arino@gmail.com>
2024-05-23 11:11:22 +0200

    [Refactor] Remove non-useful comments and try except block used for debugging

--- dags/load_noticias_to_mongodb_dag.py
+++ dags/load_noticias_to_mongodb_dag.py
@@ -26,7 +26,6 @@ def load_to_mongodb():
     db = client["noticias_db"]
     collection = db["noticias"]
 
-    # Read the transformed JSON files from the rich folder
     with open(
         OUTPUT_PATH,
         "r",
@@ -34,34 +33,27 @@ def load_to_mongodb():
     ) as f:
         data = json.load(f)
         for item in data:
-            # Check if news with the same title already exist (if so, update them)
             existing_news = collection.find_one({"title": item["title"]})
             if existing_news:
-                # Update the existing news item
                 collection.update_one({"title": item["title"]}, {"$set": item})
             else:
-                # Insert the new news item
                 collection.insert_one(item)
 
 
-try:
-    with DAG(
-        dag_id="load_noticias_to_mongodb_dag",
-        schedule_interval="0 9 * * 1",
-        default_args=default_args,
-        catchup=False,
-    ) as dag:
-        sentiment_analysis_task = SparkSubmitOperator(
-            task_id="run_sentiment_analysis",
-            application="jobs/sentiment_analysis.py",
-            conn_id="spark-conn",
-            verbose=True,
-        )
-        load_to_mongodb_task = (
-            load_to_mongodb()
-        )  # pylint: disable=invalid-name
-        # pylint: disable=pointless-statement
-        sentiment_analysis_task >> load_to_mongodb_task
-except Exception as e:
-    print(f"An error occurred:\n {e}")
-    raise e
+
+with DAG(
+    dag_id="load_noticias_to_mongodb_dag",
+    schedule_interval="0 9 * * 1",
+    default_args=default_args,
+    catchup=False,
+) as dag:
+    sentiment_analysis_task = SparkSubmitOperator(
+        task_id="run_sentiment_analysis",
+        application="jobs/sentiment_analysis.py",
+        conn_id="spark-conn",
+        verbose=True,
+    )
+    # pylint: disable=invalid-name
+    load_to_mongodb_task = load_to_mongodb()
+    # pylint: disable=pointless-statement
+    sentiment_analysis_task >> load_to_mongodb_task
